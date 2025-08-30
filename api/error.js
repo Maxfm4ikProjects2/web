c3127cbc4022f839ed1e60818be6cfc5519173c8
Maxfm4ik <max@maxfm4ik.site>
2025-08-30 12:55:53 +0200

    Comment out error handler in error.js

--- api/error.js
+++ api/error.js
@@ -1,3 +1,5 @@
-export default function handler(req, res) {
-  res.status(500).send('Internal Server Error');
-}
+// export default function handler(req, res) {
+//  res.status(500).send('Internal Server Error');
+// }
+
+// USED ONLY TO SHUTDOWN WEBSITE DURING UPDATE
